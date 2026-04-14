8eedecaa96e594454ab49684cdaa30c14d372910
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2026-04-14 10:31:59 +0200

    iwuegf

--- Program.cs
+++ Program.cs
@@ -14,43 +14,70 @@
 
             return liste;
         }
-        static int frequenza(List<int> liste)
+        static List<int> Rotazione(List<int> liste, int spostamenti)
+        {
+            for(int i = 0;  i < spostamenti; i++)
+            {
+                liste.Insert(0, liste[liste.Count - 1]);
+                liste.RemoveAt(5);
+            }
+
+            return liste;
+        }
+        static void frequenza(List<int> liste)
         {
             int a = 0, max = -10000, num = 0;
 
             for(int i = 0; i < liste.Count; i++)
             {
+                a = 0;
+
                 for(int j = 0; j < liste.Count; j++)
                 {
                     if (liste[i] == liste[j])
                     {
                         a++;
-                        num = j;
+                        
                     }
                 }
 
                 if(max < a)
                 {
                     max = a;
+                    num = i;
                 }
             }
+
+            Console.Write("Il numero " + liste[num] + " è il piu ripetuto. ");
+            Console.WriteLine("Si ripete " + max + " volte");
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("Esercizio 1");
             List<int> valori = new List<int>() { 0, 5, 2, 8, 4, 10, 6, 7 };
             IndiceEsplosivo(valori);
 
-            Console.Write("Esercizio 1");
             foreach (int i in valori)
             {
                 Console.WriteLine(i);
             }
 
+            Console.WriteLine("Esercizio 2");
+            Console.WriteLine("Dimmi il numero di spostamenti");
+            int spostamenti = Convert.ToInt32(Console.ReadLine());
 
+            List<int> Numeri = new List<int>() { 1, 2, 3, 4, 5 };
+            Rotazione(Numeri, spostamenti);
 
+            foreach (int i in Numeri)
+            {
+                Console.Write(i + " ");
+            }
 
-
-            List<int> NumeriR = new List<int>() { 4, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9 };
+            Console.WriteLine();
+            Console.WriteLine("Esercizio 3");
+            List<int> NumeriR = new List<int>() { 4, 2, 4, 2, 3, 2, 4, 1, 2, 4, 2 };
+            frequenza(NumeriR);
         }
     }
 }
